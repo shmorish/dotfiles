d9d75992159a27c8015c56a063180fc704ac0256
sh-morishita <sh-morishita@kddi-agdc.com>
2026-01-02 22:54:42 +0900

    check if Oh My Zsh is already installed

--- .bin/scripts/oh-my-zsh.sh
+++ .bin/scripts/oh-my-zsh.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 install_ohmyzsh() {
+  if [ -d "$HOME/dotfiles/oh-my-zsh" ]; then
+    echo "Oh My Zsh is already installed."
+    return
+  fi
   ZSH="$HOME/dotfiles/oh-my-zsh" REPO="shmorish/ohmyzsh" \
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/shmorish/ohmyzsh/master/tools/install.sh)" "" --unattended
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/dotfiles/oh-my-zsh/custom}/plugins/zsh-autosuggestions
