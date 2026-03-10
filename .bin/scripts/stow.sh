c8ebf3796aa8a91b36137ab71ad59d444b38b06b
shmorish <110565242+shmorish@users.noreply.github.com>
2026-03-10 10:30:37 +0900

    Neovimの設定を更新し、プラグインの追加と設定を行いました。
    - stow.shから_symlinks削除関数を削除し、unset_symlinks_with_stowを呼び出すように変更
    - Neovimの初期設定ファイルを新規作成し、基本設定とプラグインの読み込みを追加
    - lazy-lock.jsonを新規作成し、プラグインの依存関係を管理
    - autocmd.luaを新規作成し、オートコマンドを設定
    - base.luaを新規作成し、基本的なオプションを設定
    - diagnostic.luaを新規作成し、診断設定を追加
    - init_lazy.luaを新規作成し、lazy.nvimのブートストラップを追加
    - keymap.luaを新規作成し、キーマッピングを設定
    - completion.luaを新規作成し、補完プラグインの設定を追加
    - config/cmp.luaを新規作成し、nvim-cmpの設定を追加
    - config/lspconfig.luaを新規作成し、LSPの設定を追加
    - config/lualine.luaを新規作成し、lualineの設定を追加
    - config/telescope.luaを新規作成し、telescopeの設定を追加
    - config/treesitter.luaを新規作成し、treesitterの設定を追加
    - lsp.luaを新規作成し、LSP関連のプラグインを追加
    - tools.luaを新規作成し、便利なツールプラグインを追加
    - ui.luaを新規作成し、UI関連のプラグインを追加
    - undoディレクトリに.gitkeepを追加

--- .bin/scripts/stow.sh
+++ .bin/scripts/stow.sh
@@ -14,32 +14,6 @@ stow_dirs=(
     "wezterm"
 )
 
-_remove_symlinks() {
-    local files=(
-        "$HOME/.zshrc"
-        "$HOME/.gitconfig"
-        "$HOME/.vimrc"
-    )
-    local dirs=(
-        "$HOME/.config/karabiner"
-        "$HOME/.config/wezterm"
-        "$HOME/.config/git"
-        "$HOME/.config/zsh"
-    )
-    for file in "${files[@]}"; do
-        if [ -L "$file" ]; then
-            rm "$file"
-            echo -e "${GREEN}Removed symlink: $file${NC}"
-        fi
-    done
-    for dir in "${dirs[@]}"; do
-        if [ -L "$dir" ]; then
-            rm -rf "$dir"
-            echo -e "${GREEN}Removed symlink: $dir${NC}"
-        fi
-    done
-}
-
 _set_symlinks() {
     stow --dir="$HOME/dotfiles" --target="$HOME" $1
     if [ $? -eq 0 ]; then
@@ -50,7 +24,7 @@ _set_symlinks() {
 }
 
 set_symlinks_with_stow() {
-    _remove_symlinks
+    unset_symlinks_with_stow
     for dir in "${stow_dirs[@]}"; do
         _set_symlinks "$dir"
     done
