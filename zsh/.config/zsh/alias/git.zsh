b18df745a907dd00155837bc1dbecffd371cbcc3
shmorish <110565242+shmorish@users.noreply.github.com>
2026-03-19 23:11:11 +0900

    git.zshのgbnukeエイリアスのクォートを修正

--- zsh/.config/zsh/alias/git.zsh
+++ zsh/.config/zsh/alias/git.zsh
@@ -8,6 +8,6 @@ alias gp='git push'
 alias gl='git pull'
 alias gba='git branch -a'
 alias gbd='git branch -D'
-alias gbnuke='git branch | grep -v '^\*' | xargs -n 1 git branch -D'
+alias gbnuke="git branch | grep -v '^\*' | xargs -n 1 git branch -D"
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign --message "--wip-- [skip ci]"'
 
