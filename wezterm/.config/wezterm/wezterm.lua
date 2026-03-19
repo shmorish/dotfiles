347c129fff025f349047770d09968d56c0f6af5f
shmorish <110565242+shmorish@users.noreply.github.com>
2026-03-19 23:54:24 +0900

    weztermの設定を更新し、自動リロード機能を追加。タブタイトルのカスタマイズやタブバーの表示設定を改善。

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,6 +3,9 @@ local mux = wezterm.mux
 
 local config = wezterm.config_builder and wezterm.config_builder() or {}
 
+-- 自動リロード
+config.automatically_reload_config = true
+
 -- Font設定
 config.font = wezterm.font_with_fallback({
   { family = "Hack Nerd Font Mono", weight = "Bold" },
@@ -10,14 +13,65 @@ config.font = wezterm.font_with_fallback({
   { family = "Hiragino Sans", weight = "DemiBold" },
   { family = "Symbols Nerd Font Mono" },
 })
-config.font_size = 15
+config.font_size = 15.0
 config.use_ime = true
-
--- 自動リロード
-config.automatically_reload_config = true
 
 -- 不透明度
 config.window_background_opacity = 0.75
+config.macos_window_background_blur = 20
+
+-- タイトルバーを非表示
+config.window_decorations = "RESIZE"
+
+-- タブが一つしかない時に非表示
+config.hide_tab_bar_if_only_one_tab = true
+
+-- タブバーを背景と同じ色にする
+config.window_frame = {
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
+}
+config.window_background_gradient = {
+  colors = { "#000000" },
+}
+
+-- タブバーの「+」,「x」ボタンを非表示
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
+config.colors = {
+  tab_bar = {
+    inactive_tab_edge = "none",
+  },
+}
+
+-- タブのタイトルをカスタマイズ
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local background = "#5c6d74"
+  local foreground = "#FFFFFF"
+  local edge_background = "none"
+
+  if tab.is_active then
+    background = "#ae8b2d"
+    foreground = "#FFFFFF"
+  end
+  local edge_foreground = background
+
+  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+  return {
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
+end)
 
 -- 画面の最大化（起動時）
 wezterm.on("gui-startup", function()
@@ -27,9 +81,6 @@ end)
 
 -- フォントサイズ変更時にウィンドウサイズは変更しない
 config.adjust_window_size_when_changing_font_size = false
-
--- タブバーの「+」を削除
-config.show_new_tab_button_in_tab_bar = false
 
 -- ペイン分割の境界線の色
 config.colors = config.colors or {}
