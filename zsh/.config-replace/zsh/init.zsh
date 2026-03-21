271afed8b6c88a228e78368f4aa612da4bf2dafa
shmorish <110565242+shmorish@users.noreply.github.com>
2026-03-21 20:14:48 +0900

    現状zsh→old, replace→zsh

--- zsh/.config-replace/zsh/init.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-CONFIG_DIR="$HOME/.config/zsh/config"
-
-# ------------ My Configs ------------ #
-[ -d "$CONFIG_DIR" ] && for config in "$CONFIG_DIR"/*.zsh; do
-  source "$config"
-done
---
+++ zsh/.config/zsh-old/init.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# Zsh Configuration Files
+_RC_DIR="$HOME/.config/zsh"
+source "$_RC_DIR/history.zsh"
+source "$_RC_DIR/z.zsh"
+
+source "$_RC_DIR/functions/init.zsh"
+source "$_RC_DIR/alias/init.zsh"
+source "$_RC_DIR/zsh-autosuggestions/init.zsh"
